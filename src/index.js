a69fa506f913d04a3a00d43b3b9b0911077a5cee
Ryosanki <69042935+Ryosanki@users.noreply.github.com>
2021-08-01 13:57:26 +0200

    Implement Canvas, Paddle and GameLoop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+import Paddle from "/src/paddle";
+
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
